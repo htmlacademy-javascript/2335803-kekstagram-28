15ea4e4eabcbcadd3751285052e30b095aedeac5
Eldar <eldar.akhmetshin.93@mail.ru>
2023-02-25 00:43:40 +0400

    files index.html and functions.js were changed

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+// Функйия проверки длины строки
+function checkLength (txt, qnt) {
+  const result = txt.length > qnt;
+  return result;
+}
+
+// Функция проверки слова на палиндром
+
+
+
+
+
+checkLength ('fffgg', 20);
