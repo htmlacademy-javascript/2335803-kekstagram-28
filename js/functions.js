b5f3ec9f286d1951f51820be382094ae8e4ef8c1
Eldar <eldar.akhmetshin.93@mail.ru>
2023-02-25 11:03:07 +0400

    file functions.js was corrected

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,43 @@
-// Функйия проверки длины строки
-function checkLength (txt, qnt) {
-  const result = txt.length > qnt;
-  return result;
-}
+const checkLength = (txt, qnt) => txt.length > qnt;
 
-// Функция проверки слова на палиндром
+// функция проверки слова на полиндром
+const checkPolindrom = (word) => {
+  const limit = Math.trunc(word.length / 2);
+  let updatedWord = word.toLowerCase();
+  updatedWord = updatedWord.replaceAll(' ', '');
+  for (let i = 0; i < limit; i ++) {
+    if (updatedWord.at(i) !== updatedWord.at(-(i + 1))) {
+      return false;
+    }
+  }
+  return true;
+};
 
+const extractNumber = (text) => {
+  const updatedText = String(text);
+  let numbersLine = '';
+  for (let i = 0; i + 1 <= updatedText.length; i ++) {
+    if (Number.isInteger(Number(text.at(i)))) {
+      numbersLine = `${numbersLine}${text.at(i)}`;
+    }
+  }
+  return numbersLine;
+};
 
+const completeString = (oldString, minLength, additionalString) => {
+  const numberRemainingSymbols = minLength - oldString.length;
+  let newString = '';
+  if (numberRemainingSymbols >= 0) {
+    return oldString;
+  }
 
-
-
-checkLength ('fffgg', 20);
+  for (let i = 0; i < numberRemainingSymbols; i += additionalString.length) {
+    const difference = numberRemainingSymbols - i;
+    if (additionalString.length <= difference) {
+      newString = `${additionalString}${newString}`;
+    } else {
+      newString = `${additionalString.at(0, difference - 1)}${newString}`;
+    }
+  }
+  return `${newString}${oldString}`;
+};
