c274416df3077df16a3e6c9323c6e17e71d1aa62
Eldar <eldar.akhmetshin.93@mail.ru>
2023-03-03 20:28:36 +0400

    some mistakes in main.js were corrected

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
-const generateNumber = (firstValue, secondValue) => {
+const generateRandomNumber = (firstValue, secondValue) => {
   const lower = Math.min(firstValue, secondValue);
   const upper = Math.max(firstValue, secondValue);
   const result = Math.floor(Math.random() * (upper - lower + 1) + lower);
   return result;
 };
 
+const createIdGenerator = () => {
+  let lastGeneratedId = 0;
+
+  return function () {
+    lastGeneratedId ++;
+    return lastGeneratedId ;
+  };
+};
+
+
 const createRandomIdFromGenerator = (firstValue, secondValue) => {
   const previousValues = [];
 
   return function () {
-    let newValue = generateNumber(firstValue, secondValue);
+    let newValue = generateRandomNumber(firstValue, secondValue);
     if (previousValues.length >= (secondValue - firstValue + 1)) {
       return;
     }
     while (previousValues.includes(newValue)) {
-      newValue = generateNumber(firstValue, secondValue);
+      newValue = generateRandomNumber(firstValue, secondValue);
     }
     previousValues.push(newValue);
     return newValue;
@@ -22,40 +32,37 @@ const createRandomIdFromGenerator = (firstValue, secondValue) => {
 };
 
 const getInfoAboutPhotos = () => {
-  const descriptions = ['Я', 'Он', 'Отвратительный вид', 'Плохой друг', 'Какой-то челик', 'Закат', 'Тонущий корабль', 'Все кто меня бесят',
+  const DESCRIPTIONS = ['Я', 'Он', 'Отвратительный вид', 'Плохой друг', 'Какой-то челик', 'Закат', 'Тонущий корабль', 'Все кто меня бесят',
     'Кучка гавнюков!!!!', 'Клёвый кот', 'Здесь лежит труп', 'Мои гразные носки'];
 
-  const messages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.',
+  const MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.',
     'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
     'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
     'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
     'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
-  const names = ['Боб', 'Уолтер', 'Джесси', 'Памела', 'Ребека', 'Сьюзан', 'Рекс', 'Ким Чен Ын', 'Абу', 'Чендлер', 'Моника',
+  const NAMES = ['Боб', 'Уолтер', 'Джесси', 'Памела', 'Ребека', 'Сьюзан', 'Рекс', 'Ким Чен Ын', 'Абу', 'Чендлер', 'Моника',
     'Росс', 'Фиби', 'Сол Гудман', 'Амояк', 'Боб младший', 'Джесика', 'Леонардо', 'Томас', 'Крокет', 'Бони', 'Клайд', 'Пушок', 'Кекс', 'Олег'];
 
-  const borderNumber = 25;
-  const getPhotoId = createRandomIdFromGenerator(1, borderNumber);
-  const getPhotoUrl = createRandomIdFromGenerator(1, borderNumber);
-  const getLikes = createRandomIdFromGenerator(15, 200);
-  const getCommentId = createRandomIdFromGenerator(0, 10000);
-  const getNameIndex = createRandomIdFromGenerator(0, names.length - 1);
-  const photos = [];
+  const BORDER_NUMBER = 25;
+  const getPhotoId = createRandomIdFromGenerator(1, BORDER_NUMBER);
+  const getPhotoUrl = createRandomIdFromGenerator(1, BORDER_NUMBER);
+  const getCommentId = createIdGenerator();
 
-  for (let i = 0; i < borderNumber; i ++) {
-    const photoInfo = {
-      id: getPhotoId(),
-      url: `photos/${getPhotoUrl()}.jpg`,
-      description: descriptions[generateNumber(0, descriptions.length - 1)],
-      likes: getLikes(),
-      comments: {
-        id: getCommentId(),
-        avatar: `img/avatar-${generateNumber(1, 6)}.svg`,
-        message: messages[generateNumber(0, messages.length - 1)],
-        name: names[getNameIndex()]
-      }
-    };
-    photos.push(photoInfo);
-  }
-  return photos;
+  const createPhotoObject = () => ({
+    id: getPhotoId(),
+    url: `photos/${getPhotoUrl()}.jpg`,
+    description: DESCRIPTIONS[generateRandomNumber(0, DESCRIPTIONS.length - 1)],
+    likes: generateRandomNumber(15, 200),
+    comments: {
+      id: getCommentId(),
+      avatar: `img/avatar-${generateRandomNumber(1, 6)}.svg`,
+      message: MESSAGES[generateRandomNumber(0, MESSAGES.length - 1)],
+      name: NAMES[generateRandomNumber(0, NAMES.length - 1)]
+    }
+  });
+
+  const createPhotoObjects = () => Array.from({length: BORDER_NUMBER}, createPhotoObject);
+
+  return createPhotoObjects();
 };
