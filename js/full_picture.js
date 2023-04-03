54066f56bc13e99f7b37e7698e73d20319175687
Eldar <eldar.akhmetshin.93@mail.ru>
2023-04-03 23:25:47 +0400

    mistakes were corrected

--- js/full_picture.js
+++ js/full_picture.js
@@ -11,28 +11,29 @@ const commentTemplate = document.querySelector('#comment')
 const commentsCount = bigPicture.querySelector('.social__comment-count');
 const commentsLoaderButton = bigPicture.querySelector('.comments-loader');
 let displayedCommentsCount = 0;
-let commentsContainer;
+let startCommentNumber = 0;
+let arrayComments;
 
 const createCommentsElements = () => {
-  const allBigPictureComments = createElement('ul', 'social__comments');
-  let i = 0;
-  commentsContainer.forEach(({avatar, message, name}) => {
-    if (i === displayedCommentsCount - 1) {
-      return;
-    }
+  const allBigPictureComments = (startCommentNumber === 0) ? createElement('ul', 'social__comments') : document.querySelector('.social__comments');
+
+  arrayComments.slice(startCommentNumber, displayedCommentsCount).forEach(({avatar, message, name}) => {
     const newCommentTemplate = commentTemplate.cloneNode(true);
     newCommentTemplate.querySelector('.social__picture').src = avatar;
     newCommentTemplate.querySelector('.social__picture').alt = name;
     newCommentTemplate.querySelector('.social__text').textContent = message;
     allBigPictureComments.appendChild(newCommentTemplate);
-    i ++;
+
   });
+  startCommentNumber = displayedCommentsCount;
   return allBigPictureComments;
 };
 
 const renderBigPictureComments = () => {
   const previousComments = bigPicture.querySelector('.social__comments');
-  previousComments.parentNode.removeChild(previousComments);
+  if (startCommentNumber === 0) {
+    previousComments.parentNode.removeChild(previousComments);
+  }
   bigPictureSocial.insertBefore(createCommentsElements (), commentsLoaderButton);
 };
 
@@ -41,8 +42,8 @@ const getClickedPicture = (photoObjects, element) => {
   return photoObjects.find((object) => Number(picture.id) === Number(object.id));
 };
 
-const calculateCommentsQuantity = (allCommentsCount) => {
-  if (allCommentsCount < LIMIT_RENDER_COMMENTS || allCommentsCount - displayedCommentsCount < LIMIT_RENDER_COMMENTS) {
+const calculateDisplayedCommentsCount = (allCommentsCount) => {
+  if (allCommentsCount < LIMIT_RENDER_COMMENTS || allCommentsCount - displayedCommentsCount <= LIMIT_RENDER_COMMENTS) {
     displayedCommentsCount = allCommentsCount;
     commentsLoaderButton.classList.add('hidden');
   } else {
@@ -59,15 +60,15 @@ const onPictureOpenClick = (photoObjects, evt) => {
   bigPicture.querySelector('img').src = picture.url;
   bigPicture.querySelector('.likes-count').textContent = picture.likes;
   bigPicture.querySelector('.social__caption').textContent = picture.description;
-  commentsContainer = picture.comments;
-  bigPicture.querySelector('.comments-count').textContent = commentsContainer.length;
-  calculateCommentsQuantity (commentsContainer.length);
-  renderBigPictureComments(commentsContainer);
+  arrayComments = picture.comments;
+  bigPicture.querySelector('.comments-count').textContent = arrayComments.length;
+  calculateDisplayedCommentsCount (arrayComments.length);
+  renderBigPictureComments(arrayComments);
   bigPicture.classList.remove('hidden');
 };
 
 const onButtonLoadMoreComments = () => {
-  calculateCommentsQuantity (bigPicture.querySelector('.comments-count').textContent);
+  calculateDisplayedCommentsCount (bigPicture.querySelector('.comments-count').textContent);
   renderBigPictureComments ();
 };
 
@@ -75,6 +76,7 @@ const onButtonCloseClick = () => {
   document.querySelector('body').classList.remove('modal-open');
   bigPicture.classList.add('hidden');
   displayedCommentsCount = 0;
+  startCommentNumber = 0;
   bigPictureCancelButton.removeEventListener('click', onButtonCloseClick);
   commentsLoaderButton.removeEventListener('click', onButtonLoadMoreComments);
   commentsLoaderButton.classList.remove('hidden');
