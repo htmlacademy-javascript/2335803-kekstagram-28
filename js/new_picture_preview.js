39303282ab6de057de565b093a85a8f8145cc22b
Eldar <eldar.akhmetshin.93@mail.ru>
2023-04-07 20:58:59 +0400

    some mistakes were corrected

--- js/new_picture_preview.js
+++ js/new_picture_preview.js
@@ -3,12 +3,17 @@ import {FILE_TYPES} from './data.js';
 const newPicturePreview = document.querySelector('.img-upload__preview')
   .querySelector('img');
 const fileChooser = document.querySelector('.img-upload__input');
+const pictureEffectButtons = document.querySelectorAll('.effects__preview');
 
 const renderNewImage = () => {
   const file = fileChooser.files[0];
   const fileName = file.name.toLowerCase();
   if (FILE_TYPES.some((type) => fileName.endsWith(type))) {
-    newPicturePreview.src = URL.createObjectURL(file);
+    const urlFile = URL.createObjectURL(file);
+    newPicturePreview.src = urlFile;
+    pictureEffectButtons.forEach((button) => {
+      button.style.backgroundImage = `url("${urlFile}")`;
+    });
   }
 };
 
